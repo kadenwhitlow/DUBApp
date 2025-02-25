39c0df3c1091a0169471569a58257680177b764e
Kaden Whitlow <kaden.whitlow@drake.edu>
2025-02-25 16:19:28 -0600

    Updated app.py and pushed branch.

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,78 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, jsonify, request, render_template, url_for, redirect, flash, session
+from flask_restful import Resource, Api
+import requests
+import json
+from functools import wraps
 
 app = Flask(__name__, template_folder='/Users/kadenwhitlow/Downloads/DUBApp/Frontend/HTML', static_folder='/Users/kadenwhitlow/Downloads/DUBApp/Frontend')
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+
+def login_required(func):
+	@wraps(func)
+	def wrapper(*args, **kwargs):
+		if 'user' in session:
+			return func(*args, **kwargs)
+		else:
+			flash('You need to log in first!', 'error')
+			return redirect(url_for('login'))
+	return wrapper
+
+##########################################################################################################
+
+# LOGIN CODE
+
+# Check if a user exists
+def user_exists(username):
+	users = load_users()
+	return username in users
+
+# Load all users from users.json
+def load_users():
+	try:
+		with open('users.json', 'r') as f:
+			return json.load(f)
+	except FileNotFoundError:
+		return {}  
+
+# Add new user
+def add_user(username, password, interests):
+	users = load_users()
+	users[username] = {
+		"password": password,
+		"interests": interests
+	}
+	with open('users.json', 'w') as f:
+		json.dump(users, f)
+	
+##########################################################################################################
+
+@app.route('/', methods=['GET', 'POST'])
+def login():	
+	if request.method == 'POST':
+		username = request.form['username']
+		password = request.form['password']
+
+		if user_exists(username): 
+			users = load_users()
+			if username in users and users[username]['password'] == password:
+				session['user'] = username
+				news_data = get_data(username)
+				add_data_file(news_data)
+				return redirect(url_for('home'))
+			else:
+				flash('Invalid username or password. Please try again!', 'error') 
+				return render_template('login.html') 
+			
+		else:
+			flash('User does not exist. Please sign up below!', 'error') 
+			return render_template('login.html') 
+	
+	else:
+		return render_template('login.html') 
+
+def verify_login():
+    
+    return None
 
 if __name__ == '__main__':
     app.run(debug=True)
